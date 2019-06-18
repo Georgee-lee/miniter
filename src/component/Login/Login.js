68c417338101e8d381d8836d42bd5873a52ebae0
Georgee-lee <rjsahdia3@gmail.com>
2019-06-18 18:25:27 +0900

    add withRouter

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,25 +1,77 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import './Login.css';
 
-const Login = () => {
+class Login extends React.Component {
 
-  return (
-    <div className="login-container">
-      <h1>Log in to Miniter</h1>
+  state = {
+    idCheck: false,
+    pwCheck: false,
+    id: '',
+    pw: ''
+  }
 
-      <form className="login-wrap">
-        <input type="text" placeholder="Enter ID" className="user-id"/ >
-        <input type="password" placeholder="Password" className="user-pw"/ >
-        <input type="submit" className="login-btn" value="Log in"/ >
-      </form>
+  checkActive = (e, val) => {
+    if(val === 1) {
+      this.setState({
+        idCheck: true,
+        id: e.currentTarget.value
+      })
+    }
 
-      <div className="move">
-        <Link to="/signup" className="move-btn">회원가입</Link>
-        <Link to="/tweet" className="move-btn">트윗보기</Link>
+    if(val === 2) {
+      this.setState({
+        pwCheck: true,
+        pw: e.currentTarget.value
+      })
+    }
+  }
+
+  // login() {
+  //     const id = this.state.id;
+  //     const pw = this.state.pw;
+
+  //     fetch('').then()
+
+  //     //  성공시 this.props.history.push('/tweet') 이런 식으로 Router의 히스토리를 이용하여 전달
+  // }
+
+  render() {
+    return (
+      <div className="login-container">
+        <h1>Log in to Miniter</h1>
+  
+        <form className="login-wrap">
+          <input 
+            value={this.state.id}
+            type="text" 
+            placeholder="Enter ID" 
+            className="user-id" 
+            onChange={e => { this.checkActive(e, 1) }}
+          />
+
+          <input 
+            type="password" 
+            placeholder="Password" 
+            className="user-pw" 
+            onChange={(e) => { this.checkActive(e, 2) }}
+          />
+
+          <input 
+            // onClick={this.login}
+            type="submit"
+            className={this.state.pwCheck && this.state.idCheck ? "login-btn active" : "login-btn"}
+            value="Log in"
+          />
+        </form>
+
+        <div className="move">
+          <Link to="/signup" className="move-btn">회원가입</Link>
+          <Link to="/tweet" className="move-btn">트윗보기</Link>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default Login;
+export default withRouter(Login);
